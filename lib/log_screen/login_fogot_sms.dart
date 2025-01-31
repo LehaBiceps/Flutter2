2b40acc4efd346934207107b0abb1a9141f4425e
LehaBiceps <alexey005v@gmail.com>
2025-01-31 12:56:53 +0700

    1.1

--- lib/log_screen/login_fogot_sms.dart
+++ lib/log_screen/login_fogot_sms.dart
@@ -5,9 +5,14 @@ import 'package:project_two/widgets/text_button.dart';
 import 'package:project_two/widgets/text_field.dart';
 import 'package:project_two/widgets/app_bar.dart';
 
-class FogotSms extends StatelessWidget {
+class FogotSms extends StatefulWidget {
   const FogotSms({super.key});
 
+  @override
+  State<FogotSms> createState() => _FogotSmsState();
+}
+
+class _FogotSmsState extends State<FogotSms> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,8 +29,10 @@ class FogotSms extends StatelessWidget {
                 fontSize: 15
             ),
           ),
-          textField(text: 'Код из смс'),
-          const SizedBox(height: 50),
+          textField(text: 'Код из смс', showTimer: true, optionalText: 'Отправить ещё раз', onPressed: (){}),
+          const SizedBox(height: 10),
+          textButton(onPressed: (){Navigator.pushNamed(context, '/fogot_phone');}, text: 'Неверный номер', alignment: Alignment.topLeft),
+          const SizedBox(height: 10),
           customButton(text: 'Продолжить', onPressed: (){}),
         ],
       ),
--- lib/widgets/text_field.dart
+++ lib/widgets/text_field.dart
@@ -1,51 +1,91 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-Widget textField(
-    {
-      required String text,
-      String? optionalText,
-      VoidCallback? onPressed,
-    })
+Widget textField({
+  required String text,
+  String? optionalText,
+  VoidCallback? onPressed,
+  bool showTimer = false,
+}) {
+  int remainingTime = 30;
+  bool isTimerActive = false;
+  late Timer timer;
 
-{
-  return Padding(
-    padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        TextField(
-          decoration: InputDecoration(
-            hintText: text,
-            hintStyle: const TextStyle(
-              color: Colors.grey,
-              fontWeight: FontWeight.w400,
-            ),
-            enabledBorder: const UnderlineInputBorder(
-              borderSide: BorderSide(color: Color(0xFFEE7100)),
-            ),
-          ),
-        ),
-        if (optionalText != null)
-          Padding(
-            padding: const EdgeInsets.only(top: 8),
-            child: TextButton(
-              onPressed: onPressed,
-              style: TextButton.styleFrom(
-                padding: EdgeInsets.zero,
-                minimumSize: const Size(0, 0),
-                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              ),
-              child: Text(
-                optionalText,
-                style: const TextStyle(
-                    color: Color(0xFFEE7100),
-                    fontSize: 15,
-                  fontWeight: FontWeight.w400
+  void startTimer(Function updateState) {
+    isTimerActive = true;
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      updateState(() {
+        if (remainingTime > 0) {
+          remainingTime--;
+        } else {
+          timer.cancel();
+          isTimerActive = false;
+        }
+      });
+    });
+  }
+
+  return StatefulBuilder(
+    builder: (context, setState) {
+      return Padding(
+        padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: text,
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w400,
+                ),
+                enabledBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFFEE7100)),
                 ),
               ),
             ),
-          ),
-      ],
-    ),
+            if (optionalText != null && !isTimerActive)
+              Padding(
+                padding: const EdgeInsets.only(top: 14),
+                child: TextButton(
+                  onPressed: () {
+                    if (onPressed != null) {
+                      onPressed();
+                    }
+                    if (showTimer) {
+                      startTimer(setState);
+                    }
+                  },
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    minimumSize: const Size(0, 0),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  child: Text(
+                    optionalText,
+                    style: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+            if (isTimerActive)
+              Padding(
+                padding: const EdgeInsets.only(top: 14),
+                child: Text(
+                  'Отправить ещё раз через $remainingTime секунд',
+                  style: const TextStyle(
+                    fontSize: 15,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+          ],
+        ),
+      );
+    },
   );
 }
